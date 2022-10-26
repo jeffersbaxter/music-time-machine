71479c636f87939d6675676224b6cdd0ffef9fb7
jeffersbaxter <jeffersbaxter@comcast.net>
2022-10-26 12:09:10 -0700

    Creates public playlist on Spotify

---
+++ README.md
@@ -0,0 +1,5 @@
+# 100 Days of Python ~ Day 46 - Music Time Machine
+
+### Concepts Used
+* Web Scraping
+* Spotipy API
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+SPOTIFY_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+SPOTIFY_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT = os.environ.get("SPOTIPY_REDIRECT_URI")
+BILLBOARD_URL = os.environ.get("BILLBOARD_TOP_100_URL")
 
 time_destination = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
 
-URL = f"https://www.billboard.com/charts/hot-100/{time_destination}"
+URL = f"{BILLBOARD_URL}{time_destination}"
 
 response = requests.get(url=URL)
 response.raise_for_status()
@@ -18,6 +23,36 @@ soup = BeautifulSoup(response.text, "html.parser")
 chart_results_list = soup.find(name="div", class_="chart-results-list")
 
 songs = [band.find(name="h3", class_="c-title").getText().strip() for band in chart_results_list.find_all(name="div", class_="o-chart-results-list-row-container")]
-print(songs[:10])
 
+auth_manager = SpotifyOAuth(
+    client_id=SPOTIFY_ID,
+    client_secret=SPOTIFY_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT,
+    show_dialog=True,
+    cache_path="token.txt",
+    scope="playlist-modify-public"
+)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
 
+user_id = spotify.current_user()["id"]
+
+song_uris = []
+year = time_destination.split("-")[0]
+for song in songs:
+    result = spotify.search(q=f"track:{song} year:{year}")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = spotify.user_playlist_create(
+    user=user_id,
+    name=f"{time_destination} Billboard 100",
+    public=True
+)
+
+spotify.playlist_add_items(
+    playlist_id=playlist["id"],
+    items=song_uris
+)
